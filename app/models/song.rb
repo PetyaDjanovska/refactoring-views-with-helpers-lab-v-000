4b51021a3997a3f0b63cad5825b623c78d0589a4
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-18 21:26:10 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,6 +6,7 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name=(name)
-    artist = Artist.find_or_create_by(name: name).name
+    self.artist = Artist.find_or_create_by(name: name)
+    @artist_name = artist.name
   end
 end
